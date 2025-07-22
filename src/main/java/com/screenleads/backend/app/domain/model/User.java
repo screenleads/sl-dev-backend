48ae52b6604cb7700b8ae5ffd20d8a8fcec67a6e
David Jato Lopez <jatod@segurcaixaadeslas.es>
2025-07-22 13:23:54 +0200

    fixed recursivity

--- src/main/java/com/screenleads/backend/app/domain/model/User.java
+++ src/main/java/com/screenleads/backend/app/domain/model/User.java
@@ -10,6 +10,8 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
@@ -38,6 +40,7 @@ public class User implements UserDetails {
     private String lastName;
 
     @ManyToOne
+    @JsonIgnoreProperties({"users", "devices", "advices"}) 
     private Company company;
 
     @OneToOne
