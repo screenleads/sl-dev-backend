2757c00ddaf7e4903e62a3d60ecfcf4d417778e2
David Jato Lopez <jatod@segurcaixaadeslas.es>
2025-09-18 16:23:51 +0200

    remove no used classes an create new repository

--- src/main/java/com/screenleads/backend/app/domain/repositories/AdviceVisibilityRuleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.screenleads.backend.app.domain.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.screenleads.backend.app.domain.model.AdviceVisibilityRule;
-
-public interface AdviceVisibilityRuleRepository extends JpaRepository<AdviceVisibilityRule, Long> {
-}
